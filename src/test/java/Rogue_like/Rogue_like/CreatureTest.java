c9161dd868d5c02e81abdfe54b3ed415a665d56c
DamaMaimouna <mayimounadama@gmail.com>
2018-12-05 21:53:08 +0100

    creation of CreatureTest Class

---
+++ src/test/java/Rogue_like/Rogue_like/CreatureTest.java
@@ -0,0 +1,14 @@
+package Rogue_like.Rogue_like;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreatureTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
