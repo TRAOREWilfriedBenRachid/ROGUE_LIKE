16663c414bd928abb4a11649979ee7d913e0ebf4
Andritsalama <andritsalamaramaroson@gmail.com>
2018-12-14 12:47:46 +0100

    Modification de l'ecran du gagnant

--- src/main/java/Rogue_like/Rogue_like/WinScreen.java
+++ src/main/java/Rogue_like/Rogue_like/WinScreen.java
@@ -4,10 +4,10 @@ import java.awt.event.KeyEvent;
 import asciiPanel.AsciiPanel;
 
 public class WinScreen implements Screen {
-////////ECRAN POUR GAGNANT /////
+//////// ECRAN AFFICHER POUR UN GAGNANT /////
     public void displayOutput(AsciiPanel terminal) {
         terminal.write("you are winner.", 5, 1);
-        terminal.writeCenter("-- Press entry for reload --", 22);
+        terminal.writeCenter("-- Press entr for reload --", 22);
     }
 
     public Screen respondToUserInput(KeyEvent key) {
