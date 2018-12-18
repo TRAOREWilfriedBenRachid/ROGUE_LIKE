8b89769e62f8c39466cf6fc083af9d8d469b10ee
Andritsalama <andritsalamaramaroson@gmail.com>
2018-12-18 22:03:33 +0100

    World test

--- src/test/java/Rogue_like/Rogue_like/WorldTest.java
+++ src/test/java/Rogue_like/Rogue_like/WorldTest.java
@@ -2,13 +2,32 @@ package Rogue_like.Rogue_like;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 public class WorldTest {
+	public World world;
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+		world = new WorldBuilder(90, 32, 5)
+			.makeCaves()
+			.build();
+		
+		// test if World object created
+		assertNotNull(world);
+		
+		// test width equality
+		assertEquals(90, world.width());
+		
+		// test height equality
+		assertEquals(30, world.height());
+		
+		// test depth equality
+		assertEquals(5, world.depth());
+		
+		
 	}
 
 }
